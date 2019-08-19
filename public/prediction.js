bc6d5df703a744f75b5c511a68ee0feb37d99719
Dionne Rodriguez <DionneRodriguez.SE@gmail.com>
2019-08-19 17:10:52 -0400

    added stylingto prediction page

--- public/prediction.js
+++ public/prediction.js
@@ -1,9 +1,32 @@
-
-
-
 document.getElementById("menu").addEventListener('click', function(){
 document.getElementById("navbarSupportedContent").classList.toggle("show")
 })
+
+
+window.addEventListener('load', function(){
+let teamOne = document.getElementById('teamOneName').innerText
+let teamTwo = document.getElementById('teamTwoName').innerText
+let finalScore = document.getElementById('score').innerText
+let numScore = parseInt(finalScore)
+if (finalScore > 0) {
+  document.getElementById('winner').innerHTML = teamOne
+  document.getElementById('teamOne').style.backgroundColor = "green"
+  document.getElementById('teamOne').style.width = "45%"
+    document.getElementById('teamOne').style.height = "550px"
+  document.getElementById('teamTwo').style.backgroundColor = 'red'
+}else{
+  document.getElementById('teamOne').style.backgroundColor = "red"
+  document.getElementById('teamTwo').style.backgroundColor = 'green'
+  document.getElementById('teamTwo').style.width = "45%"
+    document.getElementById('teamTwo').style.height = "550px"
+  document.getElementById('winner').innerHTML = teamTwo
+}
+document.getElementById('score').innerHTML = numScore
+})
+
+
+
+
 
 
 let bothTeams = []
